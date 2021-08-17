ce48130cf7d9e08ddd60c51f0eb73d4a26a264bd
marjanf <marjanf@devvm2298.prn0.facebook.com>
2021-08-17 15:16:16 -0700

    Adding BFP16 quantization/dequantization support to OSS
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63059
    
    Supporting BFP16 quantization method to OSS. Currently only support CPU
    ghstack-source-id: 136050570
    
    Differential Revision: [D30194538](https://our.internmc.facebook.com/intern/diff/D30194538/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D30194538/)!

--- torch/csrc/distributed/c10d/init.cpp
+++ torch/csrc/distributed/c10d/init.cpp
@@ -17,6 +17,7 @@
 
 #ifdef USE_C10D_NCCL
 #include <c10d/ProcessGroupNCCL.hpp>
+#include <torch/csrc/distributed/c10d/quantization/quantization_gpu.h>
 #endif
 
 #ifdef USE_C10D_MPI
@@ -31,8 +32,10 @@
 #include <c10d/frontend.hpp>
 #include <c10d/logger.hpp>
 #include <c10d/reducer.hpp>
+
 #include <torch/csrc/Exceptions.h>
 #include <torch/csrc/distributed/c10d/python_comm_hook.h>
+#include <torch/csrc/distributed/c10d/quantization/quantization.h>
 #include <torch/csrc/jit/python/pybind_utils.h>
 #include <torch/csrc/utils/object_ptr.h>
 #include <torch/csrc/utils/pybind.h>
@@ -1643,6 +1646,28 @@ static PyMethodDef methods[] = { // NOLINT
 PyMethodDef* python_functions() {
   return methods;
 }
+
+namespace quantization {
+TORCH_LIBRARY(q, m) {
+    m.def("Bfloat16QuantizedToFloat(Tensor input) -> Tensor");
+    m.def("FloatToBfloat16Quantized(Tensor input) -> Tensor");
+}
+
+TORCH_LIBRARY_IMPL(q, CPU, m) {
+    m.impl("Bfloat16QuantizedToFloat", _bfloat16_to_float_cpu);
+    m.impl("FloatToBfloat16Quantized", _float_to_bfloat16_cpu);
+}
+
+#ifdef USE_C10D_NCCL
+    #define DISPATCH_TO_CUDA(name, function) \
+        m.impl(name, torch::dispatch(c10::DispatchKey::CUDA, TORCH_FN(function)))
+    TORCH_LIBRARY_IMPL(q, CUDA, m) {
+        DISPATCH_TO_CUDA("FloatToBfloat16Quantized", _float_to_bfloat16_gpu);
+        DISPATCH_TO_CUDA("Bfloat16QuantizedToFloat", _bfloat16_to_float_gpu);
+    }
+#endif
+
+} // namespace quantization
 
 } // namespace c10d
 } // namespace distributed
