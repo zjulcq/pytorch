efccc54f3f3ce436a5098249411cb6db09879516
marjanf <marjanf@devvm2298.prn0.facebook.com>
2021-08-18 19:45:23 -0700

    Adding BFP16 quantization/dequantization support to OSS
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63059
    
    Supporting BFP16 quantization method to OSS. Currently only support CPU
    ghstack-source-id: 136179498
    
    Differential Revision: [D30194538](https://our.internmc.facebook.com/intern/diff/D30194538/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D30194538/)!

--- test/distributed/algorithms/quantization/test_quantization.py
+++ test/distributed/algorithms/quantization/test_quantization.py
@@ -6,6 +6,7 @@ import torch.distributed.algorithms.quantization.quantization as quant
 from torch.distributed.algorithms.quantization.quantization import DQuantType
 from torch.testing._internal.common_distributed import (
     requires_gloo,
+    skip_if_rocm,
     skip_if_lt_x_gpu,
     requires_nccl,
 )
@@ -18,9 +19,9 @@ def _build_tensor(size, value=None, dtype=torch.float, device_id=None):
     if value is None:
         value = size
     if device_id is None:
-        return torch.empty(size, size, size, dtype=dtype).fill_(value)
+        return torch.empty(size, dtype=dtype).fill_(value)
     else:
-        return torch.empty(size, size, size, dtype=dtype).fill_(value).cuda(device_id)
+        return torch.empty(size, dtype=dtype).fill_(value).cuda(device_id)
 
 class DistQuantizationTests(TestDistBackend, DistributedTest._DistTestBase):
     def setUp(self):
@@ -33,9 +34,16 @@ class DistQuantizationTests(TestDistBackend, DistributedTest._DistTestBase):
         group, group_id, rank = self._init_global_test()
         self._test_all_gather(group, group_id, rank, dtype=torch.float32, qtype=DQuantType.FP16)
 
+    @requires_gloo()
+    @sandcastle_skip_if(BACKEND != "gloo", "Only gloo backend supports all_gather_bfp16")
+    def test_all_gather_bfp16(self):
+        group, group_id, rank = self._init_global_test()
+        self._test_all_gather(group, group_id, rank, dtype=torch.float32, qtype=DQuantType.BFP16)
+
     @requires_nccl()
     @sandcastle_skip_if(BACKEND != "nccl", "Only nccl backend supports all_to_all_fp16")
     @skip_if_lt_x_gpu(int(os.environ["WORLD_SIZE"]))
+    @skip_if_rocm
     def test_all_to_all_fp16(self):
         group, group_id, rank = self._init_global_test()
         rank_to_GPU = self._init_multigpu_helper()
@@ -48,16 +56,34 @@ class DistQuantizationTests(TestDistBackend, DistributedTest._DistTestBase):
             dtype=torch.float32,
             qtype=DQuantType.FP16)
 
+    # @requires_nccl()
+    # @sandcastle_skip_if(BACKEND != "nccl", "Only nccl backend supports all_to_all_bfp16")
+    # @skip_if_lt_x_gpu(int(os.environ["WORLD_SIZE"]))
+    # @skip_if_rocm
+    # def test_all_to_all_bfp16(self):
+    #     group, group_id, rank = self._init_global_test()
+    #     rank_to_GPU = self._init_multigpu_helper()
+    #     self._test_all_to_all(
+    #         group,
+    #         group_id,
+    #         rank,
+    #         cuda=True,
+    #         rank_to_GPU=rank_to_GPU,
+    #         dtype=torch.float32,
+    #         qtype=DQuantType.BFP16)
+
     def _test_all_gather(
-            self, group, group_id, rank, cuda=False, rank_to_GPU=None, dtype=torch.float, qtype=None):
+            self, group, group_id, rank, cuda=False, rank_to_GPU=None, dtype=torch.float, qtype=DQuantType.FP16):
         for dest in group:
-            tensor = _build_tensor(dest + 1, rank, dtype=dtype)
-            tensors = [_build_tensor(dest + 1, -1, dtype=dtype) for i in group]
-            expected_tensors = [_build_tensor(dest + 1, i, dtype=dtype) for i in group]
-            if (qtype is not None):
-                allgather = quant.auto_quantize(dist.all_gather, qtype, quant_loss=None)
-            else:
-                allgather = dist.all_gather
+            tensor = _build_tensor([dest + 1, dest + 1, dest + 1], rank, dtype=dtype)
+            tensors = [_build_tensor([dest + 1, dest + 1, dest + 1], -1, dtype=dtype) for i in group]
+            expected_tensors = [_build_tensor([dest + 1, dest + 1, dest + 1], i, dtype=dtype) for i in group]
+            if (qtype == DQuantType.BFP16):
+                tensor = _build_tensor([dest + 1, dest + 1], rank, dtype=dtype)
+                tensors = [_build_tensor([dest + 1, dest + 1], -1, dtype=dtype) for i in group]
+                expected_tensors = [
+                    _build_tensor([dest + 1, dest + 1], i, dtype=dtype) for i in group
+                ]
             if cuda:
                 tensor = tensor.cuda(rank_to_GPU[rank][0])
                 tensors = [t.cuda(rank_to_GPU[rank][0]) for t in tensors]
@@ -65,6 +91,7 @@ class DistQuantizationTests(TestDistBackend, DistributedTest._DistTestBase):
                 tensor_shapes = [torch.view_as_real(tensors[0]).shape]
             else:
                 tensor_shapes = [tensors[0].shape]
+            allgather = quant.auto_quantize(dist.all_gather, qtype, quant_loss=None)
             allgather(tensors, tensor, group=group_id, async_op=False)
 
             for t1, t2 in zip(tensors, expected_tensors):
@@ -80,7 +107,7 @@ class DistQuantizationTests(TestDistBackend, DistributedTest._DistTestBase):
         cuda=False,
         rank_to_GPU=None,
         dtype=torch.float,
-        qtype=None
+        qtype=DQuantType.FP16
     ):
         if group_id is not None:
             size = len(group)
@@ -101,11 +128,8 @@ class DistQuantizationTests(TestDistBackend, DistributedTest._DistTestBase):
                     t.cuda(rank_to_GPU[rank][0]) for t in expected_tensors
                 ]
                 out_tensors = [t.cuda(rank_to_GPU[rank][0]) for t in out_tensors]
-            if(qtype is not None):
-                quantize_alltoall = quant.auto_quantize(dist.all_to_all, qtype, quant_loss=None)
-                quantize_alltoall(out_tensors, in_tensors, group=group_id)
-            else:
-                dist.all_to_all(out_tensors, in_tensors, group=group_id)
+            quantize_alltoall = quant.auto_quantize(dist.all_to_all, qtype, quant_loss=None)
+            quantize_alltoall(out_tensors, in_tensors, group=group_id)
             for t1, t2 in zip(out_tensors, expected_tensors):
                 self.assertEqual(t1, t2)
         self._barrier()
